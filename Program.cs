5ea7d479735f052f54148925b1a8b42a272022b9
Francisco Martillo <franciscom119@gmail.com>
2019-07-11 19:59:37 -0400

    Se removió Hello World y se agrego variables con valores para realizar una suma.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
